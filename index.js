97aa2e681f44e5225f79fbaa779405cf53cc6040
anubhavbhardwaj2004-sketch <anubhavbhardwaj2004@gmail.com>
2025-09-13 13:23:52 +0530

    translate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const speakBtn = document.getElementById('speakBtn');
 const clearBtn = document.getElementById('clearBtn');
 const messageBox = document.getElementById('messageBox');
-
+const toInput = document.getElementById('toInput');
 // SpeechRecognition setup
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 recognition.lang = 'en-US';
@@ -25,4 +25,23 @@ recognition.onend = () => {
 clearBtn.onclick = () => {
   messageBox.value = '';
 };
+document.getElementById('Hindi-btn').onclick = () => {
+  const message = messageBox.value;
+  if (!message) {
+    alert('Please speak a message first!');
+  }
+  else{
+    // Simple hardcoded translation for demonstration
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+recognition.lang = 'hi-IN';
+    
+    recognition.onresult = (event) => {
+      toInput.value = event.results[0][0].transcript;
+    };
+    recognition.start();
+    recognition.onend = () => {
+      speakBtn.textContent = '🎤';
+    };
 
+  }
+}
